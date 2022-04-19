132b694a58697e700bc9f10013fd1e91d1e74b5b
demostudent17 <102067912+demostudent17@users.noreply.github.com>
2022-04-19 15:26:57 -0500

    Lock down edit (#1)
    * Hide edit and delete icons in photo partial
    
    * Locked down photo controller with before_actions
    
    * random change

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,16 @@
 class PhotosController < ApplicationController
+  # random change
+  
   before_action :set_photo, only: %i[ show edit update destroy ]
+
+  before_action :ensure_owner_is_current_user, only: [:edit, :update, :destroy]
+
+  def ensure_owner_is_current_user
+    if @photo.owner != current_user
+      # redirect_to root_path, alert: "Nice try, suckah"
+      redirect_back fallback_location: root_path, alert: "Nice try, suckah"
+    end
+  end
 
   # GET /photos or /photos.json
   def index
@@ -51,6 +62,7 @@ class PhotosController < ApplicationController
   # DELETE /photos/1 or /photos/1.json
   def destroy
     @photo.destroy
+
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
       format.json { head :no_content }
